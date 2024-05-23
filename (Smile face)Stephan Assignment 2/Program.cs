c0cea26c3625338dd41712407910d9986d662d91
Stephan <wearshark13@gmail.com>
2024-05-23 08:35:10 -0400

    apply visual studio project

---
+++ (Smile face)Stephan Assignment 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _Smile_face_Stephan_Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
